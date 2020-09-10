06d5858956384043ccd1a465d0ffa601c89e6729
Anton S <anton@sokolovski.com>
2020-09-11 00:48:34 +0300

    deploy

---
+++ precache-manifest.d37f7a45bbf8154bdd74c86635dfecc3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2afe68e6459544ef8519",
+    "url": "/workout-v-2/css/app.192561a5.css"
+  },
+  {
+    "revision": "6433eec9cdea1fa58408",
+    "url": "/workout-v-2/css/chunk-vendors.e33783dd.css"
+  },
+  {
+    "revision": "46e5471b0d295a084092d2926db24ef2",
+    "url": "/workout-v-2/index.html"
+  },
+  {
+    "revision": "2afe68e6459544ef8519",
+    "url": "/workout-v-2/js/app.8ac29f42.js"
+  },
+  {
+    "revision": "6d2d669444d7c7dba656",
+    "url": "/workout-v-2/js/chunk-29c88e42.a0f6bdcb.js"
+  },
+  {
+    "revision": "2d9a9c6b0ba248661a56",
+    "url": "/workout-v-2/js/chunk-2d0a4d89.a7d7ca6e.js"
+  },
+  {
+    "revision": "5049044979fabd510f9b",
+    "url": "/workout-v-2/js/chunk-34120208.f7e9a25b.js"
+  },
+  {
+    "revision": "b36369b97d4274310d2a",
+    "url": "/workout-v-2/js/chunk-5f4ad635.a25d588d.js"
+  },
+  {
+    "revision": "64cbc6042de2d13d6134",
+    "url": "/workout-v-2/js/chunk-7809807e.51062956.js"
+  },
+  {
+    "revision": "139bb233f7450da22ced",
+    "url": "/workout-v-2/js/chunk-a04b3394.88768171.js"
+  },
+  {
+    "revision": "6433eec9cdea1fa58408",
+    "url": "/workout-v-2/js/chunk-vendors.491e3df5.js"
+  },
+  {
+    "revision": "2f9e5dc93ba9124970eccd6250182280",
+    "url": "/workout-v-2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/workout-v-2/robots.txt"
+  }
+]);
